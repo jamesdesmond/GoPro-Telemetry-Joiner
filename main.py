bd3b86a9c393878eee7b0fe4ea6104b8f905be29
James Desmond <jamesdesmond13@gmail.com>
2020-08-20 09:39:47 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+
+# Got these column names from GoProDataMergeProgram
+acc_cols = ['GlobalTime','Total Ascent Meters','Total Descent Meters','Altitude Min Meters','Altitude Max Meters','Total Ascent Feet','Total Descent Feet','Altitude Min Feet','Altitude Max Feet','Distance Feet','Distance Meters','Distance Miles','Distance Kilometers']
+
+final_df = pd.DataFrame(columns=acc_cols)
+path = 'input/'
+fileList = os.listdir(path)
+for file in fileList:
+    with pd.option_context('display.precision', 10):
+        temp_df = pd.read_csv(path + file, float_precision=None)
+    for acc_col in final_df[acc_cols]:
+        # check if final_df already has a value to add, this is only not true for the first CSV parsed
+        if len(final_df[acc_col].index) != 0:
+            # save value to add as the last value in each column
+            value_to_add = final_df[acc_col].iloc[-1]
+            # now we must add value_to_add to temp_df[acc_col]
+            temp_df[acc_col] = temp_df[acc_col] + value_to_add
+    final_df = final_df.append(temp_df)
+final_df = final_df[acc_cols]
+final_df.to_csv('merged_output.csv', index=False)
